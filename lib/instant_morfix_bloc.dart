c51e423b440888d647f10804c50a1a6b3247b969
Ido Weiss <idow09@gmail.com>
2019-02-10 23:13:21 +0200

    First naive implementation of the BLoC

--- lib/instant_morfix_bloc.dart
+++ lib/instant_morfix_bloc.dart
@@ -1,3 +1,36 @@
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:rxdart/rxdart.dart';
+
 class InstantMorfixBLoC {
-  void dispose() {}
+  InstantMorfixBLoC() {
+    _searchController.stream.listen((query) async {
+      List<Result> x = searchFor(query);
+      _results.add(x);
+    });
+  }
+
+  final StreamController<String> _searchController = StreamController<String>();
+  final BehaviorSubject<List<Result>> _results =
+      BehaviorSubject<List<Result>>();
+
+  Sink<String> get search => _searchController.sink;
+
+  Stream<UnmodifiableListView<Result>> get results =>
+      _results.stream.map((list) => UnmodifiableListView(list));
+
+  void dispose() {
+    _searchController.close();
+  }
+
+  List<Result> searchFor(String query) {
+    return new List.of([Result("Hi"), Result("Bye")]);
+  }
+}
+
+class Result {
+  final String word;
+
+  Result(this.word);
 }
