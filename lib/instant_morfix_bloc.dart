e6b04d58c9577fce70e1bdd7c8360d25895591d0
Ido Weiss <idow09@gmail.com>
2019-02-27 22:42:01 +0200

    Added focus support: Every time the input field get focus, the text is highlighted There is a sink you can add "focus requests" to, in order to get the field in focus again

--- lib/instant_morfix_bloc.dart
+++ lib/instant_morfix_bloc.dart
@@ -10,21 +10,27 @@ class InstantMorfixBLoC {
 
   InstantMorfixBLoC(this._api) {
     _searchController.stream.listen((query) async {
-      FullTranslation x = await searchFor(query);
-      _result.add(x);
+      _result.add(await searchFor(query));
     });
   }
 
   final StreamController<String> _searchController = StreamController<String>();
+  final StreamController<void> _focusController = StreamController<void>();
   final BehaviorSubject<FullTranslation> _result =
       BehaviorSubject<FullTranslation>();
 
   Sink<String> get search => _searchController.sink;
 
+  Sink<void> get focusRequest => _focusController.sink;
+
   Stream<FullTranslation> get result => _result.stream;
+
+  Stream<void> get focusCommand => _focusController.stream;
 
   void dispose() {
     _searchController.close();
+    _focusController.close();
+    _result.close();
   }
 
   Future<FullTranslation> searchFor(String query) async {
--- lib/main.dart
+++ lib/main.dart
@@ -73,14 +73,51 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class QueryInput extends StatelessWidget {
+class QueryInput extends StatefulWidget {
   final InstantMorfixBLoC bloc;
 
   const QueryInput(this.bloc);
 
   @override
+  _QueryInputState createState() => _QueryInputState();
+}
+
+class _QueryInputState extends State<QueryInput> {
+  final _controller = TextEditingController();
+  final _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      print('focusNode did something');
+      if (_focusNode.hasFocus) {
+        print('focusNode has focus!');
+        _controller.selection =
+            TextSelection(baseOffset: 0, extentOffset: _controller.text.length);
+      }
+    });
+
+    widget.bloc.focusCommand
+        .listen((_) => FocusScope.of(context).requestFocus(_focusNode));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
+      textInputAction: TextInputAction.done,
+      autofocus: true,
+      focusNode: _focusNode,
+      controller: _controller,
+      maxLines: 1,
+      onFieldSubmitted: (s) => widget.bloc.search.add(s),
       decoration: const InputDecoration(
         border: OutlineInputBorder(),
         hintText: 'Type in English / Hebrew',
@@ -95,9 +132,6 @@ class QueryInput extends StatelessWidget {
 //              ),
 //        ),
       ),
-      autofocus: true,
-      maxLines: 1,
-      onFieldSubmitted: (s) => bloc.search.add(s),
     );
   }
 }
