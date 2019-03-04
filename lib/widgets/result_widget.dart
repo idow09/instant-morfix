61e9a82e2b21b6f7a8b10ba078f810ff4c45f649
Ido Weiss <idow09@gmail.com>
2019-03-04 19:23:53 +0200

    Improved the result widget to support multi directionality

--- lib/widgets/result_widget.dart
+++ lib/widgets/result_widget.dart
@@ -4,53 +4,57 @@ import 'package:instant_morfix/models.dart';
 
 class ResultWidget extends StatelessWidget {
   final TranslationItem item;
+  final bool isOriginLangRtl;
 
-  ResultWidget(this.item);
+  ResultWidget(this.item, {this.isOriginLangRtl = true});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              textBaseline: TextBaseline.alphabetic,
-              children: <Widget>[
-                Text(
-                  item.partOfSpeech,
-                  textAlign: TextAlign.right,
-                  style: Theme.of(context).textTheme.body1,
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Text(
-                  item.inputMeanings[0],
-                  textAlign: TextAlign.right,
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
+    return Directionality(
+      textDirection: isOriginLangRtl ? TextDirection.rtl : TextDirection.ltr,
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    item.inputMeanings[0],
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Text(
+                    item.partOfSpeech,
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.body1,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Divider(color: Colors.black),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: item.outputMeanings.map((meaning) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  meaning,
-                  textAlign: TextAlign.left,
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              );
-            }).toList(),
-          ),
-        ],
+            Divider(color: Colors.black),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: item.outputMeanings.map((meaning) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    meaning,
+                    textAlign: TextAlign.end,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
