ecd8cd7237e1e14cf810398b5f27420ea2a512a8
Ido Weiss <idow09@gmail.com>
2019-11-18 01:41:18 +0200

    Receive intents on android!

--- android/app/src/main/java/com/weiss/ido/instantmorfix/MainActivity.java
+++ android/app/src/main/java/com/weiss/ido/instantmorfix/MainActivity.java
@@ -1,13 +1,40 @@
 package com.weiss.ido.instantmorfix;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
+    private String sharedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            }
+        }
+
+        new MethodChannel(getFlutterView(), "app.channel.shared.data").setMethodCallHandler(
+                (call, result) -> {
+                    if (call.method.contentEquals("getSharedText")) {
+                        result.success(sharedText);
+                        sharedText = null;
+                    }
+                });
+    }
+
+    void handleSendText(Intent intent) {
+        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+    }
 }
--- lib/widgets/instant_morfix_app.dart
+++ lib/widgets/instant_morfix_app.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:instant_morfix/focus_bloc.dart';
 import 'package:instant_morfix/translation_bloc.dart';
 import 'package:instant_morfix/translation_engine.dart';
 import 'package:instant_morfix/widgets/home_page.dart';
 
-class InstantMorfixApp extends StatelessWidget {
+class InstantMorfixApp extends StatefulWidget {
   final TranslationEngine translationEngine;
 
   const InstantMorfixApp({Key key, this.translationEngine})
       : assert(translationEngine != null),
         super(key: key);
+
+  @override
+  _InstantMorfixAppState createState() => _InstantMorfixAppState();
+}
+
+class _InstantMorfixAppState extends State<InstantMorfixApp> {
+  static const platform = const MethodChannel('app.channel.shared.data');
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedText();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +41,7 @@ class InstantMorfixApp extends StatelessWidget {
         providers: [
           BlocProvider<TranslationBloc>(
             builder: (context) =>
-                TranslationBloc(translationEngine: translationEngine),
+                TranslationBloc(translationEngine: widget.translationEngine),
           ),
           BlocProvider<FocusBloc>(
             builder: (context) => FocusBloc(),
@@ -36,5 +50,13 @@ class InstantMorfixApp extends StatelessWidget {
         child: HomePage(),
       ),
     );
+  }
+
+  getSharedText() async {
+    var sharedData = await platform.invokeMethod("getSharedText");
+    if (sharedData != null) {
+      BlocProvider.of<TranslationBloc>(context)
+          .add(FetchTranslation(query: sharedData));
+    }
   }
 }
