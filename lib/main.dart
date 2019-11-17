1a5371822e00c7266bfdb8d3819da971a327655a
Ido Weiss <idow09@gmail.com>
2019-11-17 22:41:24 +0200

    Introduced flutter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:instant_morfix/simple_bloc_delegate.dart';
 import 'package:instant_morfix/widgets/instant_morfix_app.dart';
 
-void main() => runApp(InstantMorfixApp());
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(InstantMorfixApp());
+}
